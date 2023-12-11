ce5c120f4d983c445d51ab8d968d0607983fa65e
Roman Yakimov <{ID}+{username}@users.noreply.github.com>
2023-12-11 17:16:56 +0100

    Refactoring.

--- main.py
+++ main.py
@@ -2,33 +2,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LightSource
 
-r = np.linspace(0, 5, 100)
-theta = np.linspace(0, 2*np.pi, 100)
-R, THETA = np.meshgrid(r, theta)
+NUM_POINTS = 100
+LIGHT_AZDEG = 315
+LIGHT_ALTDEG = 45
+PLOT_SIZE = (8, 6)
 
-# Defining the s-orbital function
-Z = (1/np.sqrt(4*np.pi))*np.exp(-R/2)
 
-# Creating a 3D plot
-fig = plt.figure(figsize=(8, 6))
-ax = fig.add_subplot(111, projection='3d')
+def s_orbital_function(r, theta_points=NUM_POINTS):
+    """Calculate s-orbital values."""
+    R, THETA = np.meshgrid(r, np.linspace(0, 2 * np.pi, theta_points))
+    Z = (1 / np.sqrt(4 * np.pi)) * np.exp(-R / 2)
+    return R, THETA, Z
 
-# Adding lighting
-ls = LightSource(azdeg=315, altdeg=45)
-colors = ls.shade(Z, cmap=plt.cm.rainbow, vert_exag=0.1, blend_mode='soft')
 
-# Drawing the surface with lighting
-surf = ax.plot_surface(R*np.cos(THETA), R*np.sin(THETA), Z, facecolors=colors, cmap='rainbow')
+def setup_lighting(Z, azdeg=LIGHT_AZDEG, altdeg=LIGHT_ALTDEG,
+                   cmap=plt.cm.rainbow):
+    """Set up lighting for the plot."""
+    ls = LightSource(azdeg=azdeg, altdeg=altdeg)
+    colors = ls.shade(Z, cmap=cmap, vert_exag=0.1, blend_mode='soft')
+    return colors
 
-# Setting the appearance of the axes
-ax.grid(False)
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-ax.set_title('s-orbital')
 
-# Adding a color scale
-plt.colorbar(surf, shrink=0.5, aspect=5)
+def create_3d_plot(R, THETA, Z, colors, size=PLOT_SIZE):
+    """Create and display a 3D plot."""
+    fig = plt.figure(figsize=size)
+    ax = fig.add_subplot(111, projection='3d')
+    surf = ax.plot_surface(R * np.cos(THETA), R * np.sin(THETA), Z,
+                           facecolors=colors, cmap='rainbow')
+    ax.grid(False)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    ax.set_title('s-orbital')
+    plt.colorbar(surf, shrink=0.5, aspect=5)
+    plt.show()
 
-# Displaying the plot
-plt.show()
+
+# Main execution
+r = np.linspace(0, 5, NUM_POINTS)
+R, THETA, Z = s_orbital_function(r)
+colors = setup_lighting(Z)
+create_3d_plot(R, THETA, Z, colors)
